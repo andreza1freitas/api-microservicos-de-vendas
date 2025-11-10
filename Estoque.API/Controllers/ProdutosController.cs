ba6cc01ecb4e54d7e729e65a29022e8a115b812b
andreza.freitas <andreza1freitas@gmail.com>
2025-11-10 20:50:55 -0300

    feat(estoque): Implementação do CRUD RESTful para Produtos (GET e POST)

---
+++ Estoque.API/Controllers/ProdutosController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Estoque.API.Data;
+using Estoque.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Estoque.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly EstoqueDbContext _context;
+
+        public ProdutosController(EstoqueDbContext context)
+        {
+            _context = context;
+        }
+
+        // POST api/produtos
+        [HttpPost]
+        public async Task<ActionResult<Produto>> CadastrarProduto(Produto produto)
+        {
+            _context.Produtos.Add(produto);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(ConsultarProdutoPorId), new { id = produto.Id }, produto);
+        }
+
+        // GET api/produtos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Produto>>> ConsultarProdutos()
+        {
+            return await _context.Produtos.ToListAsync();
+        }
+
+        // GET api/produtos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Produto>> ConsultarProdutoPorId(int id)
+        {
+            var produto = await _context.Produtos.FindAsync(id);
+
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return produto;
+        }
+    }
+}
